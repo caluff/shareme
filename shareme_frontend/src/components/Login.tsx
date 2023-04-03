6b0e42f8d9bc4f10001059d66b7c37fa883dac08
Yoana <yoana@cybertouch.co>
2023-04-02 23:32:31 -0400

    error login con google

--- shareme_frontend/src/components/Login.tsx
+++ shareme_frontend/src/components/Login.tsx
@@ -6,23 +6,20 @@ import logo from '../assets/logocaluff.png'
 import {client} from "../client";
 
 const Login = () => {
-    const responseGoogle = (  response:any ) => {
-        const navigate = useNavigate();
-
+    const navigate = useNavigate();
+    const responseGoogle = (response: any) => {
         localStorage.setItem('user', JSON.stringify(response.profileObj));
-
         const {name, googleId, imageUrl} = response.profileObj;
-
         const doc = {
             _id: googleId,
             _type: 'user',
             userName: name,
             image: imageUrl,
         }
-         client.createIfNotExists(doc)
-             .then(() => {
-                 navigate('/',{replace:true})
-             })
+        client.createIfNotExists(doc)
+            .then(() => {
+                navigate('/', {replace: true})
+            })
     }
     return (
         <div className="flex justify-start items-center flex-col h-screen">
